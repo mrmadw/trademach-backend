98a092e6690596b52d103fef3bef9734ff009a09
Solomon Ivankin <solomoni@sonoma.ac.pg>
2026-01-04 21:00:09 +1000

    Updated Permission classes Again

--- api/views.py
+++ api/views.py
@@ -13,6 +13,7 @@ from rest_framework_simplejwt.views import (TokenRefreshView, TokenObtainPairVie
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
+from rest_framework.permissions import AllowAny
 
 
 #class GoogleLogin(SocialLoginView):
